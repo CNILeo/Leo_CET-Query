ddf7beb8a1ef22bdc6ef902a36df0c3098214a25
CNILeo <leo@stu.tjpu.edu.cn>
2018-03-01 02:26:53 +0800

    3.1

--- CET_CHSI_0.2.1.py
+++ CET_CHSI_0.2.1.py
@@ -13,10 +13,10 @@ import random
 import socket
 import struct
 
-print '本程序来自https://github.com/CNILeo/Leo_CET-Query'
-print '     版权归TJPU-Leo所有，严禁用于商业用途！      '
-print '        如在使用本软件的过程中出现报错           '
-print '            可加入QQ群362699550反馈              '
+print ('本程序来自https://github.com/CNILeo/Leo_CET-Query')
+print ('     版权归TJPU-Leo所有，严禁用于商业用途！      ')
+print ('        如在使用本软件的过程中出现报错           ')
+print ('            可加入QQ群362699550反馈              ')
 
 HEADERS = {
     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:58.0) Gecko/20100101 Firefox/58.0',
@@ -30,14 +30,14 @@ param={
         'xm':''}
 
 xxdm = 120040 #请自行修改学校代码
-type = 1 #四级修改为1，六级修改为2
-kc = 1 #考场默认从1开始，可以自行修改
+type = 2 #四级修改为1，六级修改为2
+kc = 83 #考场默认从1开始，可以自行修改
 zwh = 1 #座位号默认从医开始，可以自行修改
 zwh_gd = 0 #确认座位号的请把0修改为1
 zkzh = (((xxdm*1000 + 172)*10+ type)*1000 + kc) * 100 + zwh #切勿修改此处
 param['zkzh']=zkzh
 print (param['zkzh'])
-param['xm']='修改此处,不要删除单引号' #单引号内修改为自己的姓名
+param['xm']='郑海生' #单引号内修改为自己的姓名
 
 while 1:
     IP = socket.inet_ntoa(struct.pack('>I', random.randint(1, 0xffffffff)))
